c14a99f9412e1ffca032f0a2cfc98d75735dabad
Zachary Johnson <zjohnson@amscreen.com>
2024-06-10 09:41:27 +0100

    User changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, RouterModule, CommonModule, FormsModule],
+  providers: [HttpClientModule],
 })
 export class AppComponent {
-  title = 'CodePulse';
+  colorFrequency: any[] = [];
+  agesPlus20: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService
+      .getUserData()
+      .subscribe((data: { colorFrequency: any[]; agesPlus20: any[] }) => {
+        this.colorFrequency = data.colorFrequency;
+        this.agesPlus20 = data.agesPlus20;
+      });
+  }
 }
